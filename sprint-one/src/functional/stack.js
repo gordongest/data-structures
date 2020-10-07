f89058719709d06280c1e0aea2486ab3c40fc89f
gordongest <gordon.gest@gmail.com>
2020-10-07 15:45:41 -0500

    first part of stack passing tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,28 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  let keyVal = 0;
+
+  var keyIncrementor = () => { keyVal++; };
+
+  var keyDecrementor = () => { keyVal--; };
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[keyVal] = value;
+    keyIncrementor(keyVal);
+    // console.log(storage);
   };
 
   someInstance.pop = function() {
+    delete storage[keyVal];
+    keyDecrementor(keyVal);
+    // console.log(storage);
+    return storage[keyVal];
   };
 
   someInstance.size = function() {
+    return Math.max(keyVal, 0);
   };
 
   return someInstance;
