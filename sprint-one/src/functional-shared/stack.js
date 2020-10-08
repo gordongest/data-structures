8aa0a6f4b9625521fc197d53991136d007c8e6eb
gordongest <gordon.gest@gmail.com>
2020-10-08 10:23:50 -0500

    working on functional-shared 'stack', trying to get access to correct properties

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newStack = {};
+
+  newStack.keyVal = 0;
+  newStack.stackSize = 0;
+
+  extend(newStack, stackMethods);
+
+  console.log(newStack);
+  console.log(newStack.push);
+  return newStack;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = (val) => {
+  // console.log(this);
+  this[this.keyVal] = val;
+  this.keyVal++;
+  this.size++;
+};
 
+stackMethods.pop = () => {
+  delete this[this.keyVal];
+  this.keyVal--;
+  this.size--;
+};
+
+stackMethods.size = () => {
+  return Math.max(this.stackSize, 0);
+};
