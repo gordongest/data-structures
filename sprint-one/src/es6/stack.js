8da827feec4b501ec6fd0c6fe4c632dacee9ac8c
gordongest <gordon.gest@gmail.com>
2020-10-08 13:37:49 -0500

    es6/stack somplete

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+
+    this.storage = {};
+    this.keyVal = 0;
+    this.stackSize = 0;
+
   }
 
+  push(val) {
+    this.storage[this.keyVal] = val;
+    this.keyVal++;
+    this.stackSize++;
+  }
+
+  pop() {
+    delete this.storage[this.keyVal];
+    this.keyVal--;
+    this.stackSize--;
+    return this.storage[this.keyVal];
+  }
+
+  size() {
+    return Math.max(this.stackSize,0);
+  }
 }
