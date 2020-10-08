0669de8e151f95a4fa79c4f38c1b02361c9f0827
gordongest <gordon.gest@gmail.com>
2020-10-08 12:26:42 -0500

    functional-shared/stack complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,40 +1,31 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
 
   var newStack = {};
 
+  newStack.storage = {};
   newStack.keyVal = 0;
   newStack.stackSize = 0;
 
-  extend(newStack, stackMethods);
+  _.extend(newStack, stackMethods);
 
-  console.log(newStack);
-  console.log(newStack.push);
   return newStack;
-};
-
-var extend = function(to, from) {
-  for (var key in from) {
-    to[key] = from[key];
-  }
 };
 
 var stackMethods = {};
 
-stackMethods.push = (val) => {
-  // console.log(this);
-  this[this.keyVal] = val;
+stackMethods.push = function(val) {
+  this.storage[this.keyVal] = val;
   this.keyVal++;
-  this.size++;
+  this.stackSize++;
 };
 
-stackMethods.pop = () => {
-  delete this[this.keyVal];
+stackMethods.pop = function() {
+  delete this.storage[this.keyVal];
   this.keyVal--;
-  this.size--;
+  this.stackSize--;
+  return this.storage[this.keyVal];
 };
 
-stackMethods.size = () => {
+stackMethods.size = function() {
   return Math.max(this.stackSize, 0);
 };
