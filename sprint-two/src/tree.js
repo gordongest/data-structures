e66054acad3a6b89613e9967d0f8c874a25ba4ec
gordongest <gordon.gest@gmail.com>
2020-10-09 08:58:15 -0500

    pseudo coded tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -17,7 +17,18 @@ treeMethods.contains = function(target) {
 };
 
 
-
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+
+ contains: O(log n)
+*/
+
+/*
+
+start with a node
+  second node goes immediately below first node
+  when adding a node, we evaluate the new node in comparison to the previous
+  recursively evaluate new node against current nodes and branches
+    if middle value of ends, make the new parent node
+    if greater, new node goes to the right
+    if lesser, new node goes to the left
